5b5b367d3f1355e747b39e19f4cb225bfc104306
Harshad06 <harshad.shringi@gmail.com>
2021-03-29 22:28:45 +0530

    push

--- consume_api.py
+++ consume_api.py
@@ -1,2 +1,14 @@
 
- # Hello 
+import requests
+import json
+
+
+response = requests.get('https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&site=stackoverflow')
+
+for data in response.json()['items']:
+    if data['answer_count'] == 0:
+        print(data['title'])
+        print(data['link'])
+    else:
+        print('Skipped')
+    print()    
